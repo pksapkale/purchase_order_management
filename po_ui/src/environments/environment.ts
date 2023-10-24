2931ee31557812fe8217e826540b422f4733c900
Pushpendra Sapkale <116496506+pksapkale@users.noreply.github.com>
2023-10-24 22:06:45 +0530

    Updated environments in ui

--- po_ui/src/environments/environment.ts
+++ po_ui/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api_path: 'http://localhost:3000',
+  // api_path: 'http://localhost:3000',
+  api_path: 'https://cooperative-bat-kimono.cyclic.app',
 };
 
 /*
